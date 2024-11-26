d9b9d7aa5d914f8d48965334d8834987130b5897
iswangjy <iswangjy@mail.ustc.edu.cn>
2024-11-26 20:01:40 +0800

    second commit

--- exps/.ipynb_checkpoints/example4-checkpoint.sh
+++
@@ -1,5 +0,0 @@
-export CUDA_VISIBLE_DEVICES=1
-python3 main_multiDS.py --mode hete6 --dataset CS --clients 10 --alg gcfl --seed 3  > ./CS/gcfl_10_3hete6.txt
-python3 main_multiDS.py --mode hete6 --dataset CS --clients 10 --alg gcfl --seed 4  > ./CS/gcfl_10_4hete6.txt
-python3 main_multiDS.py --mode hete6 --dataset CS --clients 20 --alg gcfl --seed 3  > ./CS/gcfl_20_3hete6.txt
-python3 main_multiDS.py --mode hete6 --dataset CS --clients 20 --alg gcfl --seed 4  > ./CS/gcfl_20_4hete6.txt
